e27fae0018a0a6a068caf7d40219e0a38e589946
Samara Vassell <sgv246810@gmail.com>
2025-12-09 15:17:19 -0500

    changes nextjs app to react app

--- tripli/src/main.jsx
+++ tripli/src/main.jsx
@@ -1,21 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client/react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000'
-    })
-});
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ApolloProvider client={client}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ApolloProvider>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
